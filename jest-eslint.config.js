e441073ed77f7e56823e92f9d4338aecb788e35e
Adrien Febvay <adrien.febvay@gmail.com>
2024-10-15 15:59:29 +0200

    Configuration adjustments, ESLint ignore type declaration files

--- jest-eslint.config.js
+++ jest-eslint.config.js
@@ -3,6 +3,5 @@ module.exports = {
   runner: 'jest-runner-eslint',
   displayName: 'lint',
   testMatch: ['<rootDir>/src/**/*.ts'],
-  // Do not watch files from ESLint test suite.
-  watchPathIgnorePatterns: ['<rootDir>/.*'],
+  testPathIgnorePatterns: ['/node_modules/', '\.d\.ts$']
 };
