400e7aaf9b34c3c4335f3d93e9cef1254fe4877a
Adrien Febvay <adrien.febvay@gmail.com>
2024-10-15 15:49:56 +0200

    Remove unused Object type declaration file

--- src/types/Object.d.ts
+++
@@ -1,7 +0,0 @@
-interface ObjectConstructor {
-  /**
-   * Returns the prototype of an object.
-   * @param o The object that references the prototype.
-   */
-  getPrototypeOf(o: object | boolean | string | number | bigint): unknown;
-}
